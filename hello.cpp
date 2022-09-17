d079f97f87f506464740cbe6992a6704d4fe9e75
Mohit Sharma <mohit2605l@gmail.com>
2022-09-17 16:05:16 +0530

    First Commit

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   int t;
+   cin>>t;
+   for (int i = 0; i < t; i++)
+   {
+    int x;
+    cin>>x;
+    if(x<=50) cout<<"LEFT"<<endl;
+    else cout<<"RIGHT"<<endl;
+    /* code */
+   }
+   
+}
